8ed66956750d85592168c4f74e0ccc13a15faa26
Alexis Gonelle <alexisgonelle.mobile@gmail.com>
2023-04-18 10:01:04 +0200

    Little add up in the fonction

--- module_00/ex01/Phonebook.class.cpp
+++ module_00/ex01/Phonebook.class.cpp
@@ -6,7 +6,7 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 13:52:16 by agonelle          #+#    #+#             */
-/*   Updated: 2023/04/17 18:45:34 by agonelle         ###   ########.fr       */
+/*   Updated: 2023/04/18 10:00:39 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,17 @@ PhoneBook::~PhoneBook(void) {
 }
 
 
-void PhoneBook::_add_contact(void) {
-	std::cout << "Adding a new contact." << std::endl;
+void PhoneBook::add_contact(void) {
 
-	std::string name("");
-	std::cout << "Please give the name:";
-	std::getline(std::cin, name);
-	std::cout << std::endl;
-
-	std::string f_name("");
-	std::cout << "Please give the Familly name:";
-	std::getline(std::cin, f_name);
-	std::cout << std::endl;
-	
-	std::string number("");
-	std::cout << "Please give the number:";
-	std::getline(std::cin, number);
-	std::cout << std::endl;
-
-	std::string darkest_secret("");
-	std::cout << "Please give the darkest secret:";
-	std::getline(std::cin, darkest_secret);
-	std::cout << std::endl;
 }
 
-void PhoneBook::_search_contact(void){
+void	PhoneBook::_add_contact_to_line(int index){
+}
+
+void	PhoneBook::_get_number_contact(void){
+	return (this->number_contact);
+}
+void PhoneBook::search_contact(void){
 	if (this->number_contact == 0)
 		std::cout << "No contact added" << std::endl;
 }
--- module_00/ex01/Phonebook.class.hpp
+++ module_00/ex01/Phonebook.class.hpp
@@ -6,7 +6,7 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 10:47:33 by agonelle          #+#    #+#             */
-/*   Updated: 2023/04/17 18:25:19 by agonelle         ###   ########.fr       */
+/*   Updated: 2023/04/18 09:59:24 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@
 class PhoneBook {
 	private:
 		unsigned int number_contact;
-		void _add_contact(void);
-		void _search_contact(void);
 		Contact _book[8];
+		void	_add_contact_to_line(int index);
+		void	_get_number_contact(void);
 
 	public:
 
 		PhoneBook( void );
 		~PhoneBook( void );
+		void add_contact(void);
+		void search_contact(void);
 	};
 
 #endif
