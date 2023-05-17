3838accb68ee52d90d74e4cb7d4c3031802b9d9a
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-05-17 11:15:23 +0200

    Mise a jour des tests de l'exercice 00

--- module_05/ex00/main.cpp
+++ module_05/ex00/main.cpp
@@ -3,7 +3,40 @@
 #include "Bureaucrat.hpp"
 
 int main() {
+	std::cout << "---------------------" << std::endl;
+	try {
 	Bureaucrat b("Bureaucrat", -1);
-	std::cout << b << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "---------------------" << std::endl;
+	try {
+	Bureaucrat b("Bureaucrat", 151);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "---------------------" << std::endl;
+	Bureaucrat b("Bureaucrat", 1);
+	try {
+		b.incrementGrade();
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "---------------------" << std::endl;
+	Bureaucrat b2("Bureaucrat", 150);
+	try {
+		b2.decrementGrade();
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << "---------------------" << std::endl;
+	Bureaucrat b3("Bureaucrat 3", 1);
+	std::cout << b3 << std::endl;
+	b3.decrementGrade();
+	std::cout << b3 << std::endl;
 	return (0);
 }
