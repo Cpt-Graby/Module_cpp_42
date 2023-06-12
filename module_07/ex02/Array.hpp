4c858e72775e54c56c9f66ce6099501bff7dc6dd
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-06-12 17:40:09 +0200

    push

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -1,12 +1,26 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
 
-#include <iostream>
+# include <iostream>
 
 template <typename T>
 class Array {
 	private:
-
+		const unsigned int M_SIZEARRAY;
+		T &tab;
 	public:
 		Array();
-		Array(unsigned int n);
+//		Array(unsigned int n);
+//		Array(Array const & src);
+//		Array & operator=(Array const & rhs);
+//		Array & operator[](void);
 		~Array();
-}
+//		unsigned int size(void) const;
+
+};
+
+template <typename T>
+std::ostream & operator<<(std::ostream &o, Array<T> const & v);
+
+#include "Array.tpp"
+#endif
---
+++ module_07/ex02/Array.tpp
@@ -0,0 +1,5 @@
+
+ template <typename T>
+ Array<T>::Array(): M_SIZEARRAY(0), tab(new T()) {
+
+	}
