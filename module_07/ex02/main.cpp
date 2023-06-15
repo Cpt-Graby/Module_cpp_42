71e156577ef67f561db30f3dc904bd80013d9da6
Alexis Gonelle <alexisgonelle.mobile@gmail.com>
2023-06-15 11:47:13 +0200

    Legere modification du main et du makefile

--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -2,12 +2,21 @@
 #include "Array.hpp"
 
 int main(void) {
+	unsigned int number = -3;
+	std::cout << number;
 	Array<int> b(5);
+	Array<int> cl(number);
 	for (int i = 0; i < 5; i++) {
 		b[i] = i; 
 		std::cout << b[i] << "\n";
 	}
-	std::cout << "-------\n";
+	std::cout << "-------\n size: ";
 	std::cout << static_cast<int>(b.size());
-	std::cout << "-------\n";
+	std::cout << "\n-------\n";
+	try {
+	b[7] = 6;
+	}
+	catch (const std::exception & e) {
+		std::cout << e.what() ;
+	}
 }
