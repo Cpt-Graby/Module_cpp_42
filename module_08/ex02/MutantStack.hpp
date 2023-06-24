85604d38a6edb7e3282e0699c0dd6eee5bf04d6e
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-06-25 01:44:47 +0200

    Mise a jour de l'exercice 02

---
+++ module_08/ex02/MutantStack.hpp
@@ -0,0 +1,22 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>{
+	private:
+
+	public:
+		MutantStack();
+		~MutantStack();
+		MutantStack(const MutantStack &rhs);
+		MutantStack & operator=(const MutantStack &rhs);
+
+		typedef typename Container::iterator iterator;
+
+		iterator begin();
+		iterator end();
+};
+
+#endif
