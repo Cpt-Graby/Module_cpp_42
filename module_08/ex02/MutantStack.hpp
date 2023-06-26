e2459b51cfa9bdab6a1c27708d0f11bddd066781
Alexis Gonelle <alexisgonelle.mobile@gmail.com>
2023-06-26 08:03:42 +0200

    Upgrade of the morning, removal of .tpp file

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -3,22 +3,36 @@
 
 #include <stack>
 
-template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>{
-	private:
+	template<typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+public:
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
 
-	public:
-		MutantStack();
-		~MutantStack();
-		MutantStack(const MutantStack &rhs);
-		MutantStack & operator=(const MutantStack &rhs);
+    // Constructeurs
+    MutantStack() : std::stack<T, Container>() {}
+    MutantStack(const MutantStack<T, Container>& other) : std::stack<T, Container>(other) {}
 
-		typedef typename Container::iterator iterator;
+    // Destructeur
+    ~MutantStack() {}
 
-		iterator begin();
-		iterator end();
+    // Opérateur d'assignation
+    MutantStack<T, Container>& operator=(const MutantStack<T, Container>& other)
+    {
+        if (this != &other)
+        {
+            std::stack<T, Container>::operator=(other);
+        }
+        return *this;
+    }
+
+    // Itérateurs
+    iterator begin() { return std::stack<T, Container>::c.begin(); }
+    iterator end() { return std::stack<T, Container>::c.end(); }
+
+    const_iterator begin() const { return std::stack<T, Container>::c.begin(); }
+    const_iterator end() const { return std::stack<T, Container>::c.end(); }
 };
 
-#include "MutantStack.tpp"
-
-#endif
+#endif // MUTANTSTACK_HPP
