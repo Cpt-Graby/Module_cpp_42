91aeca8ff3e6c79ca7f3e746446cfeeb6a88ad5d
Alexis Gonelle <alexisgonelle.mobile@gmail.com>
2023-06-26 13:02:46 +0200

    leger avancement sur le main

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
+
+void btcStart(char *filename);
 
 int main(int argc, char **argv) {
 	(void) argv;
 	if (argc != 2) {
-		std::cout << "Mets que un argument, toccard\n";
+		//std::cerr << "Format: " << argv[0] << " arg1\n";
+		std::cerr << "Error: could not open file.\n";
 		return (1);
 	}
+	btcStart(argv[1]);
 	return (0);
+}
+
+void btcStart(char *filename) {
+	std::cout << "The filename is: " << filename << "\n";
+	std::fstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error: could not open file.\n";
+		return ;
+	}
+
+
+	return ;
 }
