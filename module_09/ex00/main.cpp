bcf51fd9f026cbf91dec8a757dedc703937b93cc
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-06-27 17:32:03 +0200

    Modification du main

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 void btcStart(char *filename);
 
@@ -14,14 +15,66 @@ int main(int argc, char **argv) {
 	return (0);
 }
 
+bool firstCheck(std::string const &Input, char c) {
+	std::size_t foundOne = Input.find_first_of(c);
+	std::size_t foundSecond = Input.find_last_of(c);
+	if (foundOne != foundSecond || foundOne == std::string::npos)
+		return (false);
+	return (true);
+}
+
+bool DateCheck(std::string const &Input) {
+	(void) Input;
+	return (true);
+}
+
+std::map<std::string, int>  getCsvData(void) {
+	std::map<std::string, int> csvData;
+	std::fstream file("data.csv");
+	std::string line;
+	std::string date;
+	std::string input;
+	if (file.is_open()) {
+		while (getline(file, line)) {
+			if (firstCheck(line, ',') == false) {
+				std::cerr << "Not the right format\n";
+				continue ;
+			}
+			//std::cout << firstCheck(line) << "\n";
+			//std::cout << line << "\n";
+		}
+		std::cout << "end of file\n";
+		file.close();
+	}
+	return (csvData);
+}
+
 void btcStart(char *filename) {
 	std::cout << "The filename is: " << filename << "\n";
+	std::map<std::string, int> CsvData(getCsvData());
+	for (auto& i : CsvData) {
+		std::cout << i << "\n";
+	}
+	/*
+	std::string line;
+
 	std::fstream file(filename);
 	if (!file.is_open()) {
 		std::cerr << "Error: could not open file.\n";
 		return ;
 	}
-
-
+	else {
+		while (getline(file, line)) {
+			if (firstCheck(line, ',') == false) {
+				std::cerr << "Not the right format\n";
+				continue ;
+			}
+			//std::cout << firstCheck(line) << "\n";
+			//std::cout << line << "\n";
+		}
+		std::cout << "end of file\n";
+		file.close();
+	}
+	*/
 	return ;
 }
