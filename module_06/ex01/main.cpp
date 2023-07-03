4fece3e5d6cb4742d9b5d5c9e21ff9bc0cec5800
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-07-03 13:24:25 +0200

    modification du main de test ex01

--- module_06/ex01/main.cpp
+++ module_06/ex01/main.cpp
@@ -6,6 +6,7 @@ int main() {
 
 	Data t_data = {5};
 	std::cout << t_data.value << " is the data\n";
+	std::cout << &t_data.value << " is the data\n";
 	
 	Data *ptr_data = &t_data;
 
@@ -14,14 +15,17 @@ int main() {
 
 	Data *ptr_new_data = Serializer::deserialize(serial_data);
 	std::cout << ptr_new_data->value << " is the new data\n";
+	std::cout << &ptr_new_data->value << " is the new data\n";
 	
 	std::cout << "----\nTEST 2 \n----\n";
 	uintptr_t data2 = {5};
 	std::cout << data2 << " is the data\n";
+	std::cout << &data2 << " is the data\n";
 	Data *ptr2Data = Serializer::deserialize(data2);
 	std::cout << ptr2Data << " is the data\n";
 	uintptr_t serial_data2 = Serializer::serialize(ptr2Data);
 	std::cout << serial_data2 << " is the data\n";
+	std::cout << &serial_data2 << " is the data\n";
 	
 
 
